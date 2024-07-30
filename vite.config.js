79cee2427922bb4662ed575cfd97cab5ec6ba60f
Asubaev Nurzhigit <asubaevn100@gmail.com>
2024-07-30 13:34:03 +0500

    Нормальная версия!

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/stars/'
-})
+  base: "/stars/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "docs/assets",
+    rollupOptions: {
+      input: "./src/main.jsx",
+    },
+  },
+});
